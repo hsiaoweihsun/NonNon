97b5d2a30ef1bf7973bc7ce4b233f79d9dd7a1a7
hsiaoweihsun <hsiaoagustin@gmail.com>
2018-05-31 23:51:03 +0800

    ConNon

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-//非常數資料的常數指標 (內容可變 位址不可變) 
+//常數資料的非常數指標 (內容不可變 位址可變) 
 #define ARR_SIZE 5
 int main(){
 
 int i,j;
-int const a[ARR_SIZE] = {3 , 9 , 11 , 4 , 8};//常數指標 ,位置不可改 內容可以改 
+const int a[ARR_SIZE] = {3 , 9 , 11 , 4 , 8};//常數資料 位置可改 內容不可改 
 	
-		  bubo(  a,ARR_SIZE);	// a乃是常數指標  
+		  bubo(  &a,&a);
 		  
 			for(i=1;i<6;i++) {
 				
